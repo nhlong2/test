7bbf6d3c7aa86f47de39f4a777a5f4a360aebee0
esperoj <esperoj@esperoj.eu.org>
2023-07-02 14:27:37 +0700

    initgp

--- speedtest.sh
+++ speedtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-speedtest --accept-gdpr --accept-license -o speedtest.novoserve.com
+speedtest --accept-gdpr --accept-license -o speedtest.wtnet.de
 speedtest -o nyc.speedtest.clouvider.net
 speedtest -o la.speedtest.clouvider.net
 speedtest -o speedtest.singnet.com.sg
