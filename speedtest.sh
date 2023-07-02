6cf72ada9e767e17154428766158a8adf7b28263
esperoj <esperoj@esperoj.eu.org>
2023-07-02 09:47:46 +0700

    add more speedtest server

--- speedtest.sh
+++ speedtest.sh
@@ -1,2 +1,5 @@
 #!/bin/bash -e
-speedtest --accept-gdpr --accept-license
+speedtest --accept-gdpr --accept-license -o speedtest.egopvp.com
+speedtest -o ny-01.plactrix.net
+speedtest -o stosat-scla-01.sys.comcast.net
+speedtest -o sg-01.plactrix.net
