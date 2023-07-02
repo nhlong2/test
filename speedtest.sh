8749d83fce9308f733297741a0befecc4a9af92c
esperoj <esperoj@esperoj.eu.org>
2023-07-02 10:00:55 +0700

    init

--- speedtest.sh
+++ speedtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-speedtest --accept-gdpr --accept-license -o speedtest.egopvp.com
+speedtest --accept-gdpr --accept-license -o speedtestnet.server-bench.de
 speedtest -o ny-01.plactrix.net
 speedtest -o stosat-scla-01.sys.comcast.net
 speedtest -o sg-01.plactrix.net
