58570703dc81a265b4c5aa2db0e8afbe99d612aa
esperoj <esperoj@esperoj.eu.org>
2023-07-04 08:41:49 +0700

    init

--- create-cache.sh
+++ create-cache.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
-
-df -h
-mkdir -p cache
-cd cache
-ls
-echo ""
-wget -O 1GB.bin --show-progress --progress=dot:giga http://hel.icmp.hetzner.com/1GB.bin
-echo ""
-ls
+if ! test -f test.bin; then
+	rm test.bin
+fi
+head -c 10G </dev/urandom >test.bin
