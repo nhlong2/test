7b28cc3492a5c7b9f756f270647bb2951a14e6b1
esperoj <esperoj@esperoj.eu.org>
2023-08-01 19:50:13 +0700

    update

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,5 +2,5 @@
 set -Eeuxo pipefail
 curl -fLsS 'https://ipwho.de'
 df -h
-network-speed.sh -r na
+network-speed.sh
 curl -fLsS 'https://yabs.sh' | bash -s -- -is 'https://www.vpsbenchmarks.com/yabs/upload'
